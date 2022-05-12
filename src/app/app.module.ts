e741f2c46ba2c71611b63edcb838dc2d35d46f59
Christopher Graves <christopher.graves@a-lign.com>
2022-05-12 08:39:54 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,33 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatCardModule,
+    MatDividerModule,
+    AgGridModule.withComponents([]),
+    MatChipsModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatChipsModule,
+    PolicyManagementDashboardRoutingModule,
+    AngularEditorModule,
+    QuillModule,
+    MatSidenavModule,
+    PolicyTemplatesModule,
+    MatGridListModule,
+    UserIconModule,
+    GenericCommentsModule,
+    SelectedPoliciesStatusbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
